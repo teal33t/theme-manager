447ce1163c18ca66a1721eefa7bda8f4b6f62451
Saman Esmaeil <saman.2791@gmail.com>
2025-02-19 12:31:10 +0330

    feat: Theme Repositoy added

---
+++ api/src/Repository/ThemeRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Theme;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class ThemeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Theme::class);
+    }
+
+    public function findDefault(): ?Theme
+    {
+        return $this->createQueryBuilder('t')
+            ->where('t.isDefault = :isDefault')
+            ->setParameter('isDefault', true)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+
+    public function unsetAllDefault(): void
+    {
+        $this->createQueryBuilder('t')
+            ->update()
+            ->set('t.isDefault', ':isDefault')
+            ->setParameter('isDefault', false)
+            ->getQuery()
+            ->execute();
+    }
+}
