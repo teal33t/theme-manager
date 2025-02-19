471662f58d565fd1a0152e76dac0b8cffe15a900
Saman Esmaeil <saman.2791@gmail.com>
2025-02-19 13:27:14 +0330

    feat: Write a normalizer to rewrite error messages

---
+++ api/src/Normalizer/ApiErrorNormalizer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Normalizer;
+
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class ApiErrorNormalizer implements NormalizerInterface
+{
+    public function normalize($exception, string $format = null, array $context = []): array
+    {
+        return [
+            'type' => 'Error',
+            'title' => $this->getTitle($exception),
+            'status' => $exception->getStatusCode(),
+            'detail' => $exception->getMessage(),
+        ];
+    }
+
+    public function supportsNormalization($data, string $format = null, array $context = []): bool
+    {
+        return $data instanceof FlattenException;
+    }
+
+    private function getTitle(FlattenException $exception): string
+    {
+        return match ($exception->getStatusCode()) {
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            409 => 'Conflict',
+            422 => 'Unprocessable Content',
+            default => 'Internal Server Error',
+        };
+    }
+}
